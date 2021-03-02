ec87794eb5c5605e76c5283b59b72a77ef545f15
Vikrom Narula <vikrom.nar@gmail.com>
2021-03-02 16:00:52 +0700

    Fixed Linint problem and formetted code

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,16 +1,17 @@
-import React, { memo } from "react";
-import { Image, StyleSheet } from "react-native";
-
-const Logo = () => (
-	<Image source={require("../assets/golf-logo.png")} style={styles.image} />
-);
+import React, { memo } from 'react';
+import { Image, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-	image: {
-		height: 200,
-		marginBottom: 12,
-		width: 200,
-	},
+  image: {
+    height: 200,
+    marginBottom: 12,
+    width: 200,
+  },
 });
+
+const Logo = () => (
+  // eslint-disable-next-line global-require
+  <Image source={require('../assets/golf-logo.png')} style={styles.image} />
+);
 
 export default memo(Logo);
--- src/core/theme.js
+++ src/core/theme.js
@@ -1,14 +1,15 @@
-import { DefaultTheme } from "react-native-paper";
+import { DefaultTheme } from 'react-native-paper';
 
-export default {
-	...DefaultTheme,
-	colors: {
-		...DefaultTheme.colors,
-		white:"white",
-		primary: "#80D2F1", //light blue for button
-		secondary: "#7FD661", // the dark green
-		error: "#f13a59",
-		background:"#CFECC5", //the background green
-		
-	},
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    white: 'white',
+    primary: '#80D2F1',
+    secondary: '#7FD661',
+    error: '#f13a59',
+    background: '#CFECC5',
+  },
 };
+
+export default theme;
