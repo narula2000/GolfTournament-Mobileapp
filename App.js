24c3fffd214567ca57e7024af8a7987ec4079272
Chakeera <chakeera1999@gmail.com>
2021-03-02 13:45:05 +0700

    add signin

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Routes from "./src/router";
+import { Provider } from "react-native-paper";
+import {theme} from "./src/core/theme";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Main = () => (
+	<Provider theme={theme}>
+		<Routes />
+	</Provider>
+);
+
+export default Main;
