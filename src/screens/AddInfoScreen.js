8c8867babf18a05e50ac8db1a43ffedadd09ba72
Snidha19 <tasfiaahsan7@gmail.com>
2021-03-04 22:48:28 +0700

    fixed appbar

--- src/screens/AddInfoScreen.js
+++ src/screens/AddInfoScreen.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { View, StyleSheet, Image, TextInput } from 'react-native';
+import { View, StyleSheet, Image, TextInput, Dimensions } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 // import { useNavigation } from "@react-navigation/native";
-import { Appbar, Button, Card, Divider, Text, Title } from 'react-native-paper';
+import {
+  Appbar,
+  Button,
+  Card,
+  Divider,
+  Text,
+  Title,
+  Paragraph,
+} from 'react-native-paper';
 import Logo from '../components/Logo';
 import Background from '../components/Background';
 import theme from '../core/theme';
 
+const screenWidth = Dimensions.get('screen').width;
+const screenHeight = Dimensions.get('screen').height;
+
 const styles = StyleSheet.create({
   view: {
     flex: 1,
-    width: '100%',
+    width: screenWidth,
+    height: screenHeight,
     backgroundColor: theme.colors.white,
   },
   appbar: {
     backgroundColor: theme.colors.background,
     height: 110,
+    width: screenWidth,
+    flexDirection: 'row',
   },
   image: {
     height: 120,
@@ -27,46 +41,64 @@ const styles = StyleSheet.create({
   },
   appbar_card: {
     backgroundColor: theme.colors.ourgreen,
-    height: 70,
-    width: 180,
-    marginLeft: 45,
+    height: 80,
+    width: screenWidth - 200,
+    marginLeft: 15,
   },
   appbar_card_text: {
-    alignItems: 'center',
-    marginTop: 8,
+    textAlign: 'center',
+    marginTop: -5,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  appbar_card_subtext: {
     textAlign: 'center',
     fontSize: 18,
     fontWeight: 'bold',
-    justifyContent: 'center',
   },
   divider: {
     backgroundColor: theme.colors.ourgreen,
     height: 6,
   },
+  sec_view: {
+    height: screenHeight - 130,
+    justifyContent: 'space-evenly',
+  },
   info_view: {
     flexDirection: 'row',
-    marginTop: 20,
+    marginTop: 25,
   },
   info_text: {
     fontSize: 20,
+    marginLeft: 20,
   },
   counter_button: {
     backgroundColor: theme.colors.white,
     borderRadius: 10,
     marginLeft: 20,
     borderColor: theme.colors.button_border,
-    width: '2%',
+    width: 'auto',
   },
   cbtn_icon: {
     color: 'black',
     justifyContent: 'center',
   },
+  btn_view_info: {
+    flexDirection: 'row',
+    marginLeft: 10,
+    marginTop: 30,
+    justifyContent: 'space-evenly',
+  },
+  btn_change: {
+    backgroundColor: theme.colors.ourgreen,
+  },
   button: {
     backgroundColor: theme.colors.secondary,
     borderRadius: 20,
-    height: 45,
+    height: 40,
     justifyContent: 'center',
-    marginVertical: 20,
+    marginTop: 20,
+    marginVertical: 10,
     marginLeft: 90,
     width: 230,
   },
@@ -95,40 +127,115 @@ const AddInfoScreen = () => {
           <Text> S.I 9</Text>
         </View>
         <Card style={styles.appbar_card}>
-          <Text style={styles.appbar_card_text}> Chakeera Cute</Text>
-          <Text style={styles.appbar_card_text}> To Par +4</Text>
+          <Card.Content>
+            <Title style={styles.appbar_card_text}> Chakeera Cutie </Title>
+            <Paragraph style={styles.appbar_card_subtext}> To Par +4</Paragraph>
+          </Card.Content>
         </Card>
       </Appbar.Header>
       <Divider style={styles.divider} />
-      <View style={styles.info_view}>
-        <Text style={styles.info_text}> Total Stroke</Text>
+      <View style={styles.sec_view}>
+        <View style={styles.info_view}>
+          <Text style={styles.info_text}> Total Stroke</Text>
+          <Button
+            icon="minus"
+            style={styles.counter_button}
+            mode="outlined"
+            labelStyle={styles.cbtn_icon}
+            onPress={onEnterPressed}
+          >
+            {' '}
+          </Button>
+        </View>
+        <View style={styles.info_view}>
+          <Text style={styles.info_text}> Putts </Text>
+          <Button
+            icon="minus"
+            style={styles.counter_button}
+            mode="outlined"
+            labelStyle={styles.cbtn_icon}
+            onPress={onEnterPressed}
+          >
+            {' '}
+          </Button>
+          <Divider style={styles.divider} />
+        </View>
+        <View style={styles.info_view}>
+          <Text style={styles.info_text}> Sand Shots</Text>
+          <Button
+            icon="minus"
+            style={styles.counter_button}
+            mode="outlined"
+            labelStyle={styles.cbtn_icon}
+            onPress={onEnterPressed}
+          >
+            {' '}
+          </Button>
+        </View>
+        <View style={styles.info_view}>
+          <Text style={styles.info_text}> Penalties </Text>
+          <Button
+            icon="minus"
+            style={styles.counter_button}
+            mode="outlined"
+            labelStyle={styles.cbtn_icon}
+            onPress={onEnterPressed}
+          >
+            {' '}
+          </Button>
+        </View>
+        <View style={styles.info_view}>
+          <Text style={styles.info_text}> Fairways </Text>
+          <Button
+            icon="minus"
+            style={styles.counter_button}
+            mode="outlined"
+            labelStyle={styles.cbtn_icon}
+            onPress={onEnterPressed}
+          >
+            {' '}
+          </Button>
+        </View>
+        <View style={styles.btn_view_info}>
+          <Button
+            mode="contained"
+            onPress={onEnterPressed}
+            style={styles.btn_change}
+          >
+            GIR
+          </Button>
+          <Button
+            mode="contained"
+            onPress={onEnterPressed}
+            style={styles.btn_change}
+          >
+            Sand Saves
+          </Button>
+          <Button
+            mode="contained"
+            onPress={onEnterPressed}
+            style={styles.btn_change}
+          >
+            Up & DOwn
+          </Button>
+        </View>
         <Button
-          icon="minus"
-          style={styles.counter_button}
-          mode="outlined"
-          labelStyle={styles.cbtn_icon}
+          style={styles.button}
+          labelStyle={styles.buttontext}
+          mode="contained"
           onPress={onEnterPressed}
         >
-          {' '}
+          Submit
         </Button>
-        <TextInput> </TextInput>
+        <Button
+          style={styles.button}
+          labelStyle={styles.buttontext}
+          mode="contained"
+          onPress={onEnterPressed}
+        >
+          Back to Score Page
+        </Button>
       </View>
-      <Button
-        style={styles.button}
-        labelStyle={styles.buttontext}
-        mode="contained"
-        onPress={onEnterPressed}
-      >
-        Submit
-      </Button>
-      <Button
-        style={styles.button}
-        labelStyle={styles.buttontext}
-        mode="contained"
-        onPress={onEnterPressed}
-      >
-        Back to Score Page
-      </Button>
     </View>
   );
 };
